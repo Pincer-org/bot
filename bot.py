23f1d3104b780c16ccf9438591f3a459371213e2
Yohann Boniface <edhyjox@gmail.com>
2021-09-11 19:40:36 +0200

    :sparkles: About command

--- bot.py
+++ bot.py
@@ -1,11 +1,19 @@
-from time import perf_counter
-
 import dotenv
 import logging
+import random
+from time import perf_counter
 
-from pincer.client import Client
+from pincer import Client, command
+from pincer.objects import Message, Embed, InteractionFlags
+from pincer.objects.embed import EmbedThumbnail, EmbedImage
+from pincer.objects.button import ButtonStyle
 
+GUILD_ID = 881531065859190804
 logging.basicConfig(level=logging.DEBUG)
+
+
+def guild_command(**kwargs):
+    return command(**kwargs, guild=GUILD)
 
 
 class Bot(Client):
@@ -18,6 +26,43 @@ class Bot(Client):
     @Client.event
     async def on_ready(self) -> None:
         logging.info(f"Logged in as {self.bot} after {perf_counter()} seconds.")
+
+    @guild_command()
+    async def about(self) -> Embed:
+        return Embed(
+            title="Pincer - 0.6.4",
+            description=(
+                "🚀 An asynchronous python API wrapper meant to replace "
+                "discord.py\n> Snappy discord api wrapper written "
+                "with aiohttp & websockets"
+            )
+        ).add_field(
+            name="**Github Repository**",
+            value="> https://github.com/Pincer-org/Pincer"
+        ).add_field(
+            name="Website",
+            value="> https://pincer.dev"
+        ).add_field(
+            name="PyPI package",
+            value="> https://pypi.org/project/Pincer"
+        ).add_field(
+            name="ReadTheDocs",
+            value="> https://pincer.readthedocs.io"
+        ).set_footer(
+            text="The package is currently within the planning phase",
+            icon_url="https://pincer.dev/img/icon.png"
+        ).set_author(
+            name="Pincer",
+            url='https://pincer.dev',
+            icon_url="https://pincer.dev/img/icon.png"
+        ).set_thumbnail(
+            url="https://pincer.dev/img/icon.png"
+        ).set_image(
+            url=(
+                "https://repository-images.githubusercontent.com"
+                "/400871418/045ebf39-7c6e-4c3a-b744-0c3122374203"
+            )
+        )
 
 
 def main() -> None:
