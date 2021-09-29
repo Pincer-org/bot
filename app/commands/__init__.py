61c2d0f841d681ae4a6b02ee41bf6432f7f9d3a5
Yohann Boniface <edhyjox@gmail.com>
2021-09-29 20:52:52 +0200

    ✨ Adding `pan` command

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -1,8 +1,9 @@
 from .about import about_command
 from .help import help_command
+from .pan import panel_command
 from .ping import ping_command
 from .pypi_dl import pypi_dl_command
-from .say import say
+from .say import say_command
 
 
 commands = (
@@ -10,5 +11,6 @@ commands = (
     help_command,
     ping_command,
     pypi_dl_command,
-    say
+    say_command,
+    panel_command
 )
