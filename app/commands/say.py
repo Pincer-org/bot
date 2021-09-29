3bc2efcb72949ebba0c087087b7a4132d0643ae2
Yohann Boniface <edhyjox@gmail.com>
2021-09-29 20:52:33 +0200

    :bug: Fixing `say` command

--- app/commands/say.py
+++ app/commands/say.py
@@ -1,7 +1,5 @@
-from pincer import command
 from pincer.objects import MessageContext, Embed
 
 
-@command(description="Say something as the bot!")
-async def say(self, ctx: MessageContext, message: str):
+async def say_command(ctx: MessageContext, message: str):
     return Embed(description=f"{ctx.author.user.mention} said:\n{message}")
